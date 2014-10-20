289a7b6ce9b849fefc233436029cb4742df422b0
minaev.mike@gmail.com <minaev.mike@gmail.com>
2014-10-20 12:43:08 +0400

    Change from_date an to_date, they were in wrong order

--- tests/page_objects/Const.py
+++ tests/page_objects/Const.py
@@ -13,6 +13,6 @@ class Const:
     USERNAME = 'tech-testing-ha2-20'
     PASSWORD = os.environ['TTHA2PASSWORD']
     DOMAIN = '@bk.ru'
-    FROM_DATE = datetime.strptime('01.09.2015', FORMAT)
-    TO_DATE = datetime.strptime('04.02.2015', FORMAT)
+    FROM_DATE = datetime.strptime('04.02.2015', FORMAT)
+    TO_DATE = datetime.strptime('01.09.2015', FORMAT)
 
